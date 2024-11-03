bdca2966dc8afd5ddc569b119822a5cf04dea70f
David Maiden <d.maiden@hotmail.com>
2024-11-03 18:40:17 +0000

    interim

---
+++ Benchmarks/Analyser.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using Bezier;
+using Bezier.Interfaces;
+using System.Drawing;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class Analyser
+    {
+        public Point P0 { get; set; }
+        public Point P1 { get; set; }
+        public Point P2 { get; set; }
+        public Point P3 { get; set; }
+        int Intervals { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            P0 = new Point(100, 100);
+            P1 = new Point(50, 120);
+            P2 = new Point(50, 280);
+            P3 = new Point(300, 300);
+            Intervals = 10;
+        }
+
+        [Benchmark]
+        public void DoBezier()
+        {
+            ICurve sut = CurveFactory.CreateCubicBezierCurve(P0, P1, P2, P3, Intervals);
+        }
+    }
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Analyser>();
+        }
+    }
+}
